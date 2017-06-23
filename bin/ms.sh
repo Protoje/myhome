32e8811148d786c6c7bcbe851247dd1f88bfd570
Dominik Lenhardt <dominik.lenhardt@outlook.de>
2017-06-23 10:43:43 +0200

    change ms.sh

--- bin/ms.sh
+++ bin/ms.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-tmux new-window -n "${*}" "ssh $1"
+pane="$(tmux new-window -P -n "${*}" "ssh $1")"
+pane1="$pane"
+win="${pane/.*}"
+tmux select-window -t "$win"
 
 shift
 
 c=1
 while [ $# -gt 0 ]; do
     (( c++ ))
-    tmux split-window -d "ssh $1";
-    tmux select-layout tiled
-    tmux select-pane -t $c
+    pane=$(tmux split-window -P -t "$pane" -d "ssh $1")
+    tmux select-layout -t "$win" tiled
+    #tmux select-pane -t "$pane"
     shift
 done
-tmux set-window-option synchronize-panes
-tmux select-layout tiled
-tmux select-pane -t 1
+tmux set-window-option -t "$win" synchronize-panes
+tmux select-layout -t "$win" tiled
+tmux select-pane -t "$pane1"
